b184a7108ae5401d8010d89cfff5f6b7986a8b1c
Seonaid <seonaidl@gmail.com>
2014-10-01 13:07:12 -0300

    Says hello when you hit enter

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.write('This came from the script file!')
+document.write('MORE different script file!');
+
+function inputBox (first_name) {
+//	do{
+	var newMsg = prompt('Hi, ' +first_name + '. Hit enter to send. Type end to finish.');
+
+	document.write(newMsg);
+//	} while (newMsg != "end");
+
+}
+
+function sayHello (first_name, e) {
+
+	if (e.keyCode === 13) {	
+		alert('Hello, ' + first_name);
+	};
+
+}
+
+var addMessage = function(){}
+
