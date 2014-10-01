cb30887300d9221cf316cf9dfd81267fc25f4046
Seonaid <seonaidl@gmail.com>
2014-10-01 17:04:45 -0300

    return key works for putting message in display area.

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-
-
-function getMessage(){
+function getMessage() {
 // retrieve the message that is in the message box and hand it to the addMessage function
 	var currMessage = document.getElementById("msgBox").value;
-	alert(currMessage);
 	addMessage(currMessage);
 
-	document.getElementById("msgBox").value = ""
+	document.getElementById("msgBox").value = "";
 };
 
-function addMessage(msg){
+function addMessage(msg) {
+// create a new paragraph element, add the message to the paragraph and append to the displaySpace.
+
 	var p = document.createElement("p");
 	var t = document.createTextNode(msg);
 	p.appendChild(t);
 
-	document.getElementById("displaySpace").appendChild(p)
-//	alert('hi!' + msg);
-
+	document.getElementById("displaySpace").appendChild(p);
 };
 
+function checkChar (e) {
+	test = e.keyCode
+	if (test === 13){
+		getMessage();
+	}
+}
+
+// event listener for the send button 
 document.getElementById("myButton").addEventListener("click", function(){getMessage();}, false);
-//document.getElementById('chat_area').addEventListener("click", function(){chatClicked();}, false);
+
+// event listener for the textarea... listening for the enter key
+
+document.getElementById("msgBox").addEventListener("keyup", function(){checkChar(event);}, false);
