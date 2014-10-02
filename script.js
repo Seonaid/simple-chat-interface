b428a573a5f4a30bb3a2960c79d2968121fb1d16
Seonaid <seonaidl@gmail.com>
2014-10-02 14:43:43 -0300

    imaginary login function constructed for initial set of ideas. Assigns the message to the current user in the displaySpace. Still needs to have blank messages rejected.

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Check whether user is "logged in" (in fact, at this point, just whether a userName exists)
+// this will become a login script in later iterations.
+
+var userName = "";
+
+
+if (!userName) {
+	var userName = prompt("What is your name?");
+}
+
+var newText = document.createTextNode("What do you have to say for yourself, " + userName + "?");
+var something = document.getElementById("msgDescription").lastChild; 
+// alert(something);
+document.getElementById("msgDescription").replaceChild(newText, something);
+/*
+var newWords = document.getElementById("msgDescription").innerHTML;
+alert(newWords);
+*/
+
+
 function getMessage() {
 // retrieve the message that is in the message box and hand it to the addMessage function
 	var currMessage = document.getElementById("msgBox").value;
@@ -15,7 +35,7 @@ function addMessage(msg) {
 // create a new paragraph element, add the message to the paragraph and append to the displaySpace.
 
 	var p = document.createElement("p");
-	var t = document.createTextNode(msg);
+	var t = document.createTextNode(userName + ": " + msg);
 	p.appendChild(t);
 
 	document.getElementById("displaySpace").appendChild(p);
