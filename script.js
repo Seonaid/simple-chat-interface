a084c71f66014c1a1e7efce45e621c11bbdbe748
Seonaid <seonaidl@gmail.com>
2014-10-02 15:41:33 -0300

    preliminary validation finished. Doesn't display a warning if the message is blank, but at least it doesn't append it to the displaySpace now.

--- script.js
+++ script.js
@@ -12,33 +12,54 @@ var newText = document.createTextNode("What do you have to say for yourself, " +
 var something = document.getElementById("msgDescription").lastChild; 
 // alert(something);
 document.getElementById("msgDescription").replaceChild(newText, something);
-/*
+
+/* echo for debugging
 var newWords = document.getElementById("msgDescription").innerHTML;
 alert(newWords);
 */
-
 
 function getMessage() {
 // retrieve the message that is in the message box and hand it to the addMessage function
 	var currMessage = document.getElementById("msgBox").value;
 	
-	if (currMessage[0] != '/'){
-		addMessage(currMessage);
-	} else{
-		alert('command not available');
+	if (validateMessage(currMessage)) {
+		addMessage(currMessage);						// adds current message to the display space
+		document.getElementById("msgBox").value = ""; //resets value of message box to ""
+/*	} else {
+		alert('Did not call addMessage.');
+		document.getElementById("msgBox").blur(); */ // removes focus from msgBox so that further enter buttons don't pop up more alerts.
 	}
-	
-	document.getElementById("msgBox").value = "";
+
 };
 
 function addMessage(msg) {
 // create a new paragraph element, add the message to the paragraph and append to the displaySpace.
+// only gets called once the message has been retrieved and validated.
 
 	var p = document.createElement("p");
 	var t = document.createTextNode(userName + ": " + msg);
 	p.appendChild(t);
 
+// 	alert("appending" + t + "to displaySpace.")
+	
 	document.getElementById("displaySpace").appendChild(p);
+};
+
+function validateMessage (msg) {
+	// body... make sure that the message doesn't start with a '/' (and that it is not blank. <== not yet implemented)
+
+// alert("Validating message! is this many characters long: " + msg.length);
+	if(msg === "\n"){
+				return false;
+	} else{
+		if (msg[0] != '/'){
+			return true;
+
+		} else{
+			alert('command not available');
+			return false;
+		}
+	}
 };
 
 function checkChar(e) {
